0635ad36dd311ae43414cb169c4d06411dc2d1dd
Shibu Manoharan <shimanoh@wkmgb1609056.global.publicisgroupe.net>
2019-02-19 12:10:28 +0000

    initial commit

--- README.md
+++ README.md
@@ -66,3 +66,11 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/de
 ### `npm run build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
+
+
+npm i --save-dev @storybook/react
+npm i --save react react-dom
+npm i --save-dev @babel/core
+npm i --save-dev babel-loader
+
+npm run storybook
--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from 'react';
+// import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Button } from '@storybook/react/demo';
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button>Hello Button</Button>
+  ))
+  .add('with some emoji', () => (
+    <Button><span role="img" aria-label="so cool">😀 😎 👍 💯</span></Button>
+  ));   
+
+
+  storiesOf('Custom Button', module)
+  .add('with custom text', () => (
+    <Button>Hello Custom Button</Button>
+  ))
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
